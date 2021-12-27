a840bf3d77b7c4eb6bc126123f53378c076bdf56
Abdulkerim Karaman <abdulkerim.karaman@albarakatech.com>
2021-12-27 11:46:55 +0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+
+
+const URL = "https://jsonplaceholder.typicode.com/todos";
+interface Posts {
+  userId: String;
+  id: Number;
+  title: String;
+  complated: boolean;
+}
 
 function App() {
+  const [postData, setPostData] = useState<Posts[]>([])
+  useEffect(() => {
+   async function getData(){
+     const response = await axios.get<Posts[]>(URL)
+     console.log(response.data)
+     setPostData(response.data)
+   }
+
+   getData()
+  }, [])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+      {
+        postData.map(item => {
+          return <p>{item.title}</p>
+        })
+      }
+      </div>
     </div>
   );
 }
